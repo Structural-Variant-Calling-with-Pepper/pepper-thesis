10996b76b4b2f0c6d6db3aa1c1c860983509b634
fahmid111 <fahmidazim7490@gmail.com>
2024-02-28 00:35:52 +0600

    sh files

--- make_images_negative.sh
+++ make_images_negative.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/sh
-INPUT_DIR="/home/shuaib/L4T1/hg002_benchmark_files"
-OUTPUT_DIR="${PWD}/Outputs"
+INPUT_DIR="/home/fahmid/Thesis/Data"
+OUTPUT_DIR="/home/fahmid/Thesis/Output"
 
-TRUTH_BED=${INPUT_DIR}/SV_excluded.bed
+BAM_DIR="/mnt/f"
+
+# TRUTH_BED=${INPUT_DIR}/SVs_only_passed.bed
+TRUTH_BED=${INPUT_DIR}/HG002_SVs_Tier1_v0.6.bed
+TRUTH_VCF=${INPUT_DIR}/SVs_only_passed.vcf.gz
 REF=${INPUT_DIR}/hs37d5.fa
 
 # OUTPUT DIRECTORIES WHERE TRAINING EXAMPLES WILL BE SAVED
-TRAIN_OUTPUT=${INPUT_DIR}/PEPPER_TRAIN_IMAGES/high_conf_only
-TEST_OUTPUT=${INPUT_DIR}/PEPPER_TEST_IMAGES/termbreak
+TRAIN_OUTPUT=${OUTPUT_DIR}/PEPPER_TRAIN_IMAGES
+TEST_OUTPUT=${OUTPUT_DIR}/PEPPER_TEST_IMAGES
 
 THREADS=16
 
 for coverage in 50
 do
-  BAM=${INPUT_DIR}/HG002_35x_HiFi_2_GRCh37.bam
+  BAM=${BAM_DIR}/hg002_74gb/HG002_35x_HiFi_2_GRCh37.bam
 
   echo ${BAM}
 
   time pepper_variant make_images \
   -b ${BAM} \
   -f ${REF} \
-  -r 1-22\
+  -r 1:75837899-75851590\
   -rb ${TRUTH_BED} \
   -t ${THREADS} \
   -o ${TRAIN_OUTPUT} \
